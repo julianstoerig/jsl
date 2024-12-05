349e3565f412c1cf4f7d068b1fd269d248259b34
julianstoerig <julian.stoerig@gmail.com>
2024-12-05 09:00:15 +0100

    add file with common number type typedefs

---
+++ typedefs.h
@@ -0,0 +1,22 @@
+#ifndef JSL_TYPEDEFS_H
+#define JSL_TYPEDEFS_H
+
+#include <stdint.h>
+
+typedef         uint8_t  u8;
+typedef        uint16_t u16;
+typedef        uint32_t u32;
+typedef        uint64_t u64;
+
+typedef          int8_t  i8;
+typedef         int16_t i16;
+typedef         int32_t i32;
+typedef         int64_t i64;
+
+typedef           float f32;
+typedef          double f64;
+
+typedef  _Complex float c32;
+typedef _Complex double c64;
+
+#endif
