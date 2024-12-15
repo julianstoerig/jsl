b0d6c167847ba8cfef3685a43553114d5e562183
julianstoerig <julian.stoerig@gmail.com>
2024-12-15 13:47:55 +0100

    refactor to not use std assert.h, add impl guard and custom no checks flag

--- malloc_checked.h
+++ malloc_checked.h
@@ -1,11 +1,24 @@
-#include <assert.h>
-#include <stddef.h>
+#pragma once
+#include <stdlib.h>
 #include <malloc.h>
+
+#ifndef MALLOC_CHECKED_DO_CHECKS
+#define MALLOC_CHECKED_DO_CHECKS 1
+#endif
+
+#ifdef MALLOC_CHECKED_IMPLEMENTATION
+
+void internal_malloc_checked_assert(void *p) {
+    if (MALLOC_CHECKED_DO_CHECKS) {
+        if (!p) fprintf(stderr, "Failed to allocate or free memory. Aborting...");
+        exit(1);
+    }
+}
 
 #define NEW(object, type) \
     do {\
         object = malloc(sizeof(type));\
-        assert(object && "allocation failed");\
+        internal_malloc_checked_assert(object);\
     } while (0)
 
 #define DELETE(object) \
@@ -15,13 +28,16 @@
     } while (0)
 
 void *malloc_checked(int bytes) {
-    void *ptr = malloc(bytes);
-    assert(ptr);
+    void *p = malloc(bytes);
+    internal_malloc_checked_assert(p);
+    return p;
 }
 
 #define free_checked(object) \
     do {\
-        assert(object && "double free"); \
+        internal_malloc_checked_assert(object); \
         free(object); \
         object = NULL; \
     } while (0)
+
+#endif
