42808ed7da0a9217cfa163a5bb8294bfbb04f182
julianstoerig <julian.stoerig@gmail.com>
2024-12-16 19:24:59 +0100

    add arena reset operations

--- arena.h
+++ arena.h
@@ -1,8 +1,9 @@
 #ifndef JSL_ARENA_H
 #define JSL_ARENA_H
 
-#include "bit_fiddling.h"
-#include "assert.h"
+#include <assert.h>
+
+#define BIT(x) (1<<(x))
 
 #define   sizeof(x)  (ptrdiff_t)sizeof(x)
 #define  countof(a)  (sizeof(a) / sizeof(*(a)))
@@ -21,11 +22,14 @@ typedef enum {
     ArenaFlags_NoResizeTODO   = BIT(2),
 } ArenaFlags;
 
-#define new(...)            newx(__VA_ARGS__,new4,new3,new2)(__VA_ARGS__)
-#define newx(arenaPtr, b,c,d,e,...) e
-#define new2(arenaPtr, type)                  (type *)alloc(arenaPtr, sizeof(type), alignof(type), 1,     ArenaFlags_None)
-#define new3(arenaPtr, type, count)           (type *)alloc(arenaPtr, sizeof(type), alignof(type), count, ArenaFlags_None)
-#define new4(arenaPtr, type, count, flags)    (type *)alloc(arenaPtr, sizeof(type), alignof(type), count, flags)
+#define new(...) newx(__VA_ARGS__, new4, new3, new2)(__VA_ARGS__)
+#define newx(arenaPtr, b, c, d, e, ...) e
+#define new2(arenaPtr, type)                  (type *)ArenaAlloc(arenaPtr, sizeof(type), alignof(type), 1,     ArenaFlags_None)
+#define new3(arenaPtr, type, count)           (type *)ArenaAlloc(arenaPtr, sizeof(type), alignof(type), count, ArenaFlags_None)
+#define new4(arenaPtr, type, count, flags)    (type *)ArenaAlloc(arenaPtr, sizeof(type), alignof(type), count, flags)
+
+#define reset(arenaPtr)        ArenaReset(arenaPtr)
+#define resetAndZero(arenaPtr) ArenaResetAndZero(arenaPtr)
 
 #ifdef JSL_ARENA_IMPLEMENTATION
 
@@ -34,7 +38,7 @@ typedef enum {
 #endif
 
 Arena ArenaCreate(ptrdiff_t cap) {
-    REQUIRE(cap > 0);
+    assert(cap > 0);
     Arena a = {};
     a.beg = malloc(cap);
     a.rst = a.beg;
@@ -42,13 +46,15 @@ Arena ArenaCreate(ptrdiff_t cap) {
     return a;
 }
 
-void* alloc(Arena* a, ptrdiff_t size, ptrdiff_t align, ptrdiff_t count, ArenaFlags flags) {
-    REQUIRE(a);
-    REQUIRE(size > 0);
-    REQUIRE(align > 0);
-    REQUIRE(count > 0);
+void* ArenaAlloc(Arena* a, ptrdiff_t size, ptrdiff_t align, ptrdiff_t count, ArenaFlags flags) {
+    assert(false);
+    assert(true);
+    assert(a);
+    assert(size > 0);
+    assert(align > 0);
+    assert(((align - 1) & 1) == 0); // align ∈ { 2^n | n ∈ ℕ }
+    assert(count >= 0);
 
-    // bitmasking modulo, since align ∈ { 2^n | n ∈ ℕ }
     ptrdiff_t padding = (uintptr_t)a->beg & (align - 1);
     ptrdiff_t available = (uintptr_t)a->end - padding; 
     if (available < 0 || count > available / size) {
@@ -61,12 +67,16 @@ void* alloc(Arena* a, ptrdiff_t size, ptrdiff_t align, ptrdiff_t count, ArenaFla
     void *p = a->beg + padding;
     a->beg += padding + count * size;
     return (flags & ArenaFlags_NoZero) ? p : memset(p, 0, count * size);
-
-    ENSURE(a);
 }
 
-void reset(Arena* a) {
+void ArenaReset(Arena* a) {
     a->beg = a->rst;
+}
+
+
+void ArenaResetAndZero(Arena* a) {
+    a->beg = a->rst;
+    memset(a->beg, 0, a->end - a->beg);
 }
 
 #endif
