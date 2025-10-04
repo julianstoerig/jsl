f2dde3f9b99aa833a8c75046ca9dba9cace6131f
julianstoerig <julian.stoerig@gmail.com>
2025-10-04 14:31:46 +0200

    update .gitignore

--- debug/CTestTestfile.cmake
+++ debug/CTestTestfile.cmake
@@ -6,7 +6,9 @@
 # testing this directory and lists subdirectories to be tested as well.
 add_test([=[test_trivial]=] "/home/julian/scratch/jsl/debug/test_trivial")
 set_tests_properties([=[test_trivial]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/julian/scratch/jsl/src/CMakeLists.txt;10;add_test;/home/julian/scratch/jsl/src/CMakeLists.txt;0;")
-add_test([=[test_get]=] "/home/julian/scratch/jsl/debug/test_get")
-set_tests_properties([=[test_get]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/julian/scratch/jsl/src/CMakeLists.txt;13;add_test;/home/julian/scratch/jsl/src/CMakeLists.txt;0;")
+add_test([=[test_sb_push_len_index_free]=] "/home/julian/scratch/jsl/debug/test_sb_push_len_index_free")
+set_tests_properties([=[test_sb_push_len_index_free]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/julian/scratch/jsl/src/CMakeLists.txt;13;add_test;/home/julian/scratch/jsl/src/CMakeLists.txt;0;")
 add_test([=[test_hash_unhash]=] "/home/julian/scratch/jsl/debug/test_hash_unhash")
 set_tests_properties([=[test_hash_unhash]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/julian/scratch/jsl/src/CMakeLists.txt;16;add_test;/home/julian/scratch/jsl/src/CMakeLists.txt;0;")
+add_test([=[test_sb_fibonacci]=] "/home/julian/scratch/jsl/debug/test_sb_fibonacci")
+set_tests_properties([=[test_sb_fibonacci]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/julian/scratch/jsl/src/CMakeLists.txt;19;add_test;/home/julian/scratch/jsl/src/CMakeLists.txt;0;")
